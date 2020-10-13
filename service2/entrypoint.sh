c17f0781092776f98e6210b506f5336cc89e2195
NoNE <m.hamza20@icloud.com>
2020-10-13 11:17:17 +0200

    Update entrypoint.sh

--- service2/entrypoint.sh
+++ service2/entrypoint.sh
@@ -6,9 +6,9 @@ PublicIP=$(curl -s https://ipinfo.io/ip)
 PrivateIP=$(ip a | grep -E "inet.* eth")
 DNSCheck=$(dig +short google.com)
 
-[[ -z "$PublicIP" ]] && echo "Can't Reach internet" || echo "PublicIP: $PublicIP" |  tee >(cat >&2)
-[[ -z "$PrivateIP" ]] && echo "Can't get PrivateIP" || echo "PrivateIP: $PrivateIP" |  tee >(cat >&2)
-[[ -z "$DNSCheck" ]] && echo "DNS not working" || echo "DNS Working" |  tee >(cat >&2)
+[[ -z "$PublicIP" ]] && echo "Can't Reach internet" || echo "PublicIP Service2: $PublicIP" |  tee >(cat >&2)
+[[ -z "$PrivateIP" ]] && echo "Can't get PrivateIP" || echo "PrivateIP Service2: $PrivateIP" |  tee >(cat >&2)
+[[ -z "$DNSCheck" ]] && echo "DNS not working" || echo "DNS Service2 Working" |  tee >(cat >&2)
 
 sleep 2
 done
